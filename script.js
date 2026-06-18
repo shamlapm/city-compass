0d5660c8ea22e44d36cde18333d8b5ca735e31da
Ubuntu <ubuntu@ip-172-31-85-206.ec2.internal>
2026-06-18 18:38:05 +0000

    Improved UI design

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-function getLocation(){
+function getLocation() {
 
-navigator.geolocation.getCurrentPosition(
+    const output = document.getElementById("location");
 
-function(position){
+    if (navigator.geolocation) {
 
-document.getElementById("location").innerHTML =
-"Latitude: " + position.coords.latitude +
-"<br>Longitude: " + position.coords.longitude;
+        navigator.geolocation.getCurrentPosition(
 
-}
+            function(position) {
 
-);
+                output.innerHTML =
+                "<b>Location Detected Successfully!</b><br><br>" +
+                "Latitude: " + position.coords.latitude +
+                "<br>" +
+                "Longitude: " + position.coords.longitude;
 
+            },
+
+            function(error) {
+
+                output.innerHTML =
+                "Location permission denied or unavailable.";
+
+            }
+
+        );
+
+    } else {
+
+        output.innerHTML =
+        "Geolocation is not supported by this browser.";
+
+    }
 }
