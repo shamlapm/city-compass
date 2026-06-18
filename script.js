26f8f9760a3c3567b7f5c0869e3d3fc2cffb6a35
Ubuntu <ubuntu@ip-172-31-85-206.ec2.internal>
2026-06-18 19:49:00 +0000

    Enhanced CityCompass UI and geolocation feature

--- script.js
+++ script.js
@@ -1,34 +1,43 @@
 function getLocation() {
 
-    const output = document.getElementById("location");
+const output = document.getElementById("location");
 
-    if (navigator.geolocation) {
+if (navigator.geolocation) {
 
-        navigator.geolocation.getCurrentPosition(
+    navigator.geolocation.getCurrentPosition(
 
-            function(position) {
+        function(position) {
 
-                output.innerHTML =
-                "<b>Location Detected Successfully!</b><br><br>" +
-                "Latitude: " + position.coords.latitude +
-                "<br>" +
-                "Longitude: " + position.coords.longitude;
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
 
-            },
+            output.innerHTML =
+            "<b>✅ Location Detected Successfully!</b><br><br>" +
+            "Latitude: " + lat +
+            "<br>" +
+            "Longitude: " + lon +
+            "<br><br>" +
+            "<a target='_blank' href='https://www.google.com/maps?q=" +
+            lat + "," + lon +
+            "'>📍 Open in Google Maps</a>";
 
-            function(error) {
+        },
 
-                output.innerHTML =
-                "Location permission denied or unavailable.";
+        function(error) {
 
-            }
+            output.innerHTML =
+            "❌ Location permission denied or unavailable.";
 
-        );
+        }
 
-    } else {
+    );
 
-        output.innerHTML =
-        "Geolocation is not supported by this browser.";
+} else {
 
-    }
+    output.innerHTML =
+    "❌ Geolocation is not supported by this browser.";
+
+}
+
+
 }
