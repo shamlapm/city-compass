d7fca28808e7a86c1146b9508e0cc1f2114bb2c6
Ubuntu <ubuntu@ip-172-31-85-206.ec2.internal>
2026-06-24 19:40:36 +0000

    Improved location feature and UI updates

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ function getLocation() {
 
 const output = document.getElementById("location");
 
+output.innerHTML = "⏳ Getting your location...";
+
 if (navigator.geolocation) {
 
     navigator.geolocation.getCurrentPosition(
@@ -10,23 +12,24 @@ if (navigator.geolocation) {
 
             const lat = position.coords.latitude;
             const lon = position.coords.longitude;
+            const accuracy = position.coords.accuracy;
 
             output.innerHTML =
-            "<b>✅ Location Detected Successfully!</b><br><br>" +
-            "Latitude: " + lat +
-            "<br>" +
-            "Longitude: " + lon +
-            "<br><br>" +
+            "<b> Location Detected Successfully!</b><br><br>" +
+            " Latitude: " + lat + "<br>" +
+            " Longitude: " + lon + "<br>" +
+            " Accuracy: " + accuracy + " meters<br><br>" +
             "<a target='_blank' href='https://www.google.com/maps?q=" +
             lat + "," + lon +
-            "'>📍 Open in Google Maps</a>";
+            "'>Open Location in Google Maps</a>";
 
         },
 
         function(error) {
 
             output.innerHTML =
-            "❌ Location permission denied or unavailable.";
+            "<b> Unable to retrieve location.</b><br><br>" +
+            "This deployment is running on HTTP. Modern browsers require HTTPS for geolocation access.";
 
         }
 
@@ -35,7 +38,7 @@ if (navigator.geolocation) {
 } else {
 
     output.innerHTML =
-    "❌ Geolocation is not supported by this browser.";
+    " Geolocation is not supported by this browser.";
 
 }
 
